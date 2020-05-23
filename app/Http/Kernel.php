8cddcdacd945aafe2b97e4d985fb23e12b21af8e
James Ikubi <30687033+dreamcrusher001@users.noreply.github.com>
2020-05-23 19:42:15 +0300

    moved fruitcake to top

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,13 +14,15 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\Cors::class,
+        
+        
     ];
 
     /**
@@ -40,9 +42,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Fruitcake\Cors\HandleCors::class,
             'throttle:60,1',
             'bindings',
-            \Fruitcake\Cors\HandleCors::class,
+            
         ],
     ];
 
